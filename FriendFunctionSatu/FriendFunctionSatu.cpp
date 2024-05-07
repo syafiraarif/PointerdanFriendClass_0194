d65dde6ae892e55f92732cd397c7e4a21e920308
syairaarif <putrisyafira4444@gmail.com>
2024-05-07 11:11:14 +0700

    membuat class mahasiswa

---
+++ FriendFunctionSatu/FriendFunctionSatu.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+private :
+    string nama;
+public :
+    friend void setNama(mahasiswa& a, string);
+};
+
+
+int main()
+{
+    
+}
+
