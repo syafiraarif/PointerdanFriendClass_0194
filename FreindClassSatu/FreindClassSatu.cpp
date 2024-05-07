4b1202e53c0bf80ad2855883dacc7a1566e75877
syairaarif <putrisyafira4444@gmail.com>
2024-05-07 11:07:57 +0700

    membuat program utama

--- FreindClassSatu/FreindClassSatu.cpp
+++ FreindClassSatu/FreindClassSatu.cpp
@@ -41,6 +41,11 @@ void siswa::setId(int pId)
 
 int main()
 {
-
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
 }
 
