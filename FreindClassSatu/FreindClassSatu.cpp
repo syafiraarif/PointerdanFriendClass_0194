53d6fefe57f276856321d45a700294402dfdd74d
syairaarif <putrisyafira4444@gmail.com>
2024-05-07 10:56:32 +0700

    membuat class siswa dan class orang

---
+++ FreindClassSatu/FreindClassSatu.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+
+class orang
+{
+private :
+    string nama;
+public :
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private :
+    int id;
+public :
+    void setid(int pid);
+    void displayAll(orang& a);
+};
+
+int main()
+{
+    
+}
+
